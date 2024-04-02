13a617acb25b19b6c2e2897ca4677c5b2f0da2ed
Kashish Saxena <kashish.saxena@successive.tech>
2024-04-02 17:41:12 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_security/core/di/locator.dart';
 import 'package:firebase_security/core/models/environments.dart';
-import 'package:firebase_security/core/utils/firebase_options.dart';
+import 'package:firebase_security/core/utils/firebase_option.dart';
 import 'package:firebase_security/ui/screens/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
